92119c3e3d3cb3285053f7b5390be1c3a62f5029
kelvisck2 <kelvisck.cabral@ufms.br>
2023-11-17 14:43:57 -0400

    primeiro commit

---
+++ emprestimoDeLivros/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace emprestimoDeLivros.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
